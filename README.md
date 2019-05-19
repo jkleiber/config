92d6349dc734768d6fb66ad97f326547839fe8b1
Justin Kleiber <jkleiber8@gmail.com>
2019-05-18 21:44:00 -0400

    Adds vscode config files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # config
-The settings I use on different programs
+My settings repository.
+
+### VS Code
+* Custom keyboard shortcuts
+* Settings for editor display
+* Automatic extension installer (Linux only)
---
+++ vscode/install_extensions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install language extensions
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension austin.code-gnu-global
+code --install-extension vsciot-vscode.vscode-arduino
+
+# Install ROS extensions
+code --install-extension ajshort.ros
+code --install-extension ajshort.msg
+
+# Install useful utilities
+code --install-extension gruntfuggly.todo-tree
+code --install-extension eamodio.gitlens
+code --install-extension cschlosser.doxdocgen
